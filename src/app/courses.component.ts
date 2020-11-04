dbd6398723e1402a1c68a9a15a74ff85b0f6f667
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-11-04 12:31:46 +0200

    Use code inside interpolation

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-courses',
-  template: '<h2>{{title}}</h2>'
+  template: '<h2>{{ "Title: " + title}}</h2>'
 })
 export class CoursesComponent {
   title = 'List of courses';
