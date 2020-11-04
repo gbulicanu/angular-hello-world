8acafa084c7726c542568af75e7f2f7ab3e66f70
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-11-04 12:32:54 +0200

    Use method in interplation

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-courses',
-  template: '<h2>{{ "Title: " + title}}</h2>'
+  template: '<h2>{{ getTitle() }}</h2>'
 })
 export class CoursesComponent {
   title = 'List of courses';
+  getTitle(){
+    return this.title;
+  }
 }
