6e64c50821ed127cde76a312c499ec98baa9eb30
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-11-05 10:20:58 +0200

    Add authors service

---
+++ src/app/authors.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { AuthorsService } from './authors.service';
+
+describe('AuthorsService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [AuthorsService]
+    });
+  });
+
+  it('should be created', inject([AuthorsService], (service: AuthorsService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  it('should get authors', inject([AuthorsService], (service: AuthorsService) => {
+    expect(service.getAuthors().length).toBeGreaterThan(0);
+  }));
+});
