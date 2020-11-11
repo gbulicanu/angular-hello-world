a5e61bb789d4d65d153e88fc4f6cafdc667307d1
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-11-11 16:39:48 +0200

    Add code to test pipes

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
+  course = {
+    title: 'The Complete Angular Course',
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2020, 11, 11)
+  };
 
   constructor() { }
 
