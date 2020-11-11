ff367a1059a3c6ed4cfdba969098966b31acf266
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-11-11 16:26:35 +0200

    2-Way binding the right way - banana in a box

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -23,7 +23,7 @@ import { CoursesService } from './courses.service';
           (click)="onClick($event)">Save</button>
       </div>
       <br />
-      <input [value]="email" (keyup.enter)="email = $event.target.value; onEnter()" />
+      <input [(ngModel)]="email" (keyup.enter)="onEnter()" />
     </div>
   `
 })
