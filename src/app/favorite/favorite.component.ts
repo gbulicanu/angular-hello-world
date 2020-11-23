f77ec4478d298598d7e6e96c3320a38d86636b44
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-11-23 16:29:42 +0200

    Implement Favorite component

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
 
-  isFavorite = true;
+  isFavorite = false;
 
   constructor() { }
+
+  onClick($event) {
+    this.isFavorite = !this.isFavorite;
+  }
 
   ngOnInit() {
   }
