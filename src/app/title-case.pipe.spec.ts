e41caab7d7a245e123b404bbd142829cb282b90e
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-11-25 14:36:45 +0200

    Add title case pipe - add first failed unit test

---
+++ src/app/title-case.pipe.spec.ts
@@ -0,0 +1,14 @@
+import { TitleCasePipe } from './title-case.pipe';
+
+describe('TitleCasePipe', () => {
+  it('create an instance', () => {
+    const pipe = new TitleCasePipe();
+    expect(pipe).toBeTruthy();
+  });
+
+  it('should transform to title case without preposition', () => {
+    const pipe = new TitleCasePipe();
+    var result = pipe.transform('strong argue');
+    expect(result).toBe('Strong Argue');
+  });
+});
