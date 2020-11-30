4d03369085ef630b8bad2bfafc8a4f598df7e2d6
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-11-30 21:50:35 +0200

    Extract toTitleCase private method

--- src/app/title-case.pipe.ts
+++ src/app/title-case.pipe.ts
@@ -15,14 +15,18 @@ export class TitleCasePipe implements PipeTransform {
       .filter(x => x !== '');
 
     for (let i = 0; i < words.length; i++) {
-      if (!this.isPreposition(words[i]) || i === 0) {
-        words[i] = words[i].substr(0, 1).toUpperCase() + words[i].substr(1).toLowerCase();
+      if (i > 0 && this.isPreposition(words[i])) {
+        words[i] = words[i].toLowerCase();
       } else {
-        words[i] = words[i].substr(0, 1).toLowerCase() + words[i].substr(1).toLowerCase();
+        words[i] = this.toTitleCase(words[i]);
       }
     }
 
     return words.join(' ');
+  }
+
+  private toTitleCase(word: string) {
+    return word.substr(0, 1).toUpperCase() + word.substr(1).toLowerCase();
   }
 
   private isPreposition(word: string) {
